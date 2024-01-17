37c2895dbfb3471153b885ef025cb0c91f017ed5
dovil4 <134449215+dovil4@users.noreply.github.com>
2024-01-17 21:40:18 +0200

    Add files via upload

---
+++ commands.js
@@ -0,0 +1,30 @@
+//import { MAX_TIME } from './config.js';
+import { countdown } from './timer.js';
+import { textDisplayData } from './text.js';
+import { results} from './stats.js';
+
+
+DOMContentLoaded.DOMContentLoaded
+
+const statsButton = document.getElementById("stats");
+document.getElementById("changeText");
+const pauseResumeButton = document.getElementById("pause");
+document.getElementById("restart");
+const timer = document.getElementById("timer");
+document.getElementById("textInput");
+document.getElementById("quitbtn");
+document.getElementById("resume");
+document.getElementById("secondrestart");
+document.getElementById("secondquit");
+const closeStats = document.getElementById("closeStats");
+const closeEsc = document.getElementById("closeEsc");
+const modalstats = document.getElementById("modalstats");
+const modalEsc = document.getElementById("modalEsc");
+const timerText =`You have ${MAX_TIME} seconds`;
+
+
+
+
+
+
+
