bfd2f12cf6234a482f7cc70b5552b1f9272eccbd
dovil4 <134449215+dovil4@users.noreply.github.com>
2024-03-18 21:08:41 +0200

    Delete DATA directory

--- DATA/weekdays.js
+++
@@ -1,4 +0,0 @@
-
-export const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-export const fullweekDays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-export const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
